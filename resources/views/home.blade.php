e87b52d639d76f010a01989755751a2f14b5a4f6
Edoardo Lai <edoardolai96@gmail.com>
2022-03-30 17:44:59 +0200

    esercizio terminato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,23 @@
 @section('content')
     
     <main class="main_home">
-            Home page
+           <div class="container">
+               @foreach ($movies as $movie )
+               <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                      <h1>{{$movie->title}}</h1>
+                  </div>
+                  <div class="flip-card-back">
+                    <p>Original language: {{$movie->original_title}}</p>
+                    <p>Nationality: {{$movie->nationality}}</p>
+                    <p>Release date: {{$movie->date}}</p>
+                    <p>Vote: {{$movie->vote}}</p>
+                  </div>
+                </div>
+              </div>               
+              @endforeach
+           </div>
     </main>
 
 @endsection
